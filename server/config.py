16009073f4830d605376d7c849821a1ba5a3b6fd
Adeyemi Arimoro <arimoro71@gmail.com>
2023-09-17 20:40:41 -0400

    create database_url

--- server/config.py
+++ server/config.py
@@ -1,4 +1,4 @@
-
+import os
 from flask_cors import CORS
 from flask import Flask
 from flask_bcrypt import Bcrypt
@@ -11,7 +11,8 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 cors = CORS(app, resources={r'*':{'origins':'*'}})
 app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
-app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///app.db'
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL')
+# postgresql://chat_app_db_ljvm_user:1jTH7lqIlqXxnpgcIK2SAAF0TMxHMETO@dpg-ck1j75fhdsdc73d2h6hg-a.oregon-postgres.render.com/chat_app_db_ljvm
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 
