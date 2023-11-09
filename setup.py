fc35f518d83762bd4174f88f618c9ef4c0ddda09
Adeyemi Arimoro <arimoro71@gmail.com>
2023-11-08 21:04:54 -0500

    edits yml and setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,8 @@ setup(
     name='server',
     version='0.1.0',
     description='Chat/Messaging App',
-    author='Adeyemi Arimoro',
-    author_email='arimoro71@gmail.com',
-    url='http://physics.codes',
+    author='Adeyemi_Arimoro',
+    author_email='arimoro@gmail.com',
     classifiers=[
         'License :: OSI Approved :: BSD License',
         'Intended Audience :: Developers',
@@ -15,22 +14,6 @@ setup(
         'Programming Language :: Python :: 3',
     ],
     python_requires='>=3',
-    zip_safe=False,
     packages=find_packages(),
-    # find automatically:
-    # packages=find_packages(),
-    package_dir={
-        'server': 'server',
-        'server.bin': 'server/bin',
-        'server.lib': 'server/lib',
-        'server.src': 'server/src',
-        'server.instance': 'server/instance',
-        'server.migrations': 'server/migrations',
-        },
-    #
     include_package_data=True,
-    # or you can specify explicitly:
-    # package_data={
-    #     'server': ['assets/*.txt']
-    #     },
-    )
+)
